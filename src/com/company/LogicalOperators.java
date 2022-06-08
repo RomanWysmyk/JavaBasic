dcd3fb1847cb2c8585d9d3ba928157a670cdb5d2
Roman <wysmykroman@gmail.com>
2022-06-08 10:19:23 +0200

    2 commit logic operators (in progress)

--- src/com/company/LogicalOperators.java
+++ src/com/company/LogicalOperators.java
@@ -3,4 +3,21 @@ package com.company;
 public class LogicalOperators {
 
 
+
+        // ARITHMETIC OPERATORS
+
+        // +
+        int addition = 1 + 1; // =2
+
+        // -
+        int subjunction = 2 - 1; // =1
+
+        // *
+        int multiplication = 2 * 2; // =4
+
+        // /
+        int division = 10 / 5; // =2
+
+        // %
+        int restFromDivision = 11 % 5 ; // =1
 }
